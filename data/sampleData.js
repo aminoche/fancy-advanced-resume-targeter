051b110db0fa2ed186c5bddba6dfa96e4edada98
Adil Minocherhomjee <adilminocherhomjee@gmail.com>
2018-09-21 15:13:19 -0600

    updated the licence and manifest, added sampleData

---
+++ data/sampleData.js
@@ -0,0 +1,134 @@
+const adilResume = `ADIL MINOCHERHOMJEE
++1-630-881-8436
+adilminocherhomjee@gmail.com
+EXPERIENCE
+
+MONTICELLO CONSULTING GROUP	New York, NY
+Senior Consultant	March 2018 – July 2018
+•	Facilitated reporting and tracking for 6 fixed income workstreams with a $25M technology budget at Bank of America
+•	Assisted with designing and implementing process enhancements for risk managers and COOs across 7 risk programs
+•	Influenced senior technology managing directors through high-level reports and presentations
+
+BOFI FEDERAL BANK	La Jolla, CA
+Vice President, Efficiency and Corporate Performance	August 2016 – December 2016
+•	Documented the business case for the purchase of a $250k enterprise workflow tool, K2 Blackpearl
+•	Expedited the installation of the tool by gathering functional requirements and tracking implementation timeline
+•	Coordinated training scope and logistics for 20 business analysts and developers across banking functions
+
+ACCENTURE	Los Angeles, CA
+Management Consultant	August 2013 – August 2016
+•	Supported Nike’s data conversion effort for shared material library across apparel, equipment, and footwear
+•	Collaborated with Nike’s SMEs to define user stories, processes, and requirements for a PTC FlexPLM implementation
+•	Managed Caterpillar’s resource work plan by tracking status, risks/issues, and revenues/expenses for $15M program
+•	Restructured Caterpillar’s staffing model through defining outsourcing agreement for product lifecycle management services
+•	Expedited Behr’s SAP Sales and Distribution upgrade through documenting functional designs and test plans
+
+	Business Systems Analyst	August 2010 – August 2013
+•	Defined Solar Turbines’ supplier onboarding and integrated purchasing process across multiple transactional systems utilizing restricted party and Foreign Corrupt Practices Act screens
+•	Standardized Solar Turbines’ engineering processes, ensured smooth transition from standards engineering to sales order engineering/manufacturing via PTC Windchill suite and assorted CAD tools
+•	Restructured Solar Turbines’ project management processes in Belgium for budgeting capital and monitoring revenue/costs for large projects using Infor ERP LN Projects module
+•	Implemented Solar Turbines’ asset management solution for its Czech Republic Greenfield facility
+•	Designed and integrated CalPERS’ customized Java application into Oracle’s PeopleSoft accounts-receivable system which enabled the tracking of retiree payment plans
+
+EDUCATION
+FULLSTACK ACADEMY OF CODE	New York, NY
+Certificate, Computer Software Engineering	October 2018
+
+UCLA ANDERSON SCHOOL OF MANAGEMENT	Los Angeles, CA
+M.B.A., Fully Employed Program, Concentrations in Marketing and Entrepreneurship	December 2017
+•	Awards: GAP Fellow (top 5% of class in final capstone consulting project)
+•	Leadership: VP of FEMBA, Anderson Eats; Teaching Assistant, Management of Technology and Innovation
+
+INDIANA UNIVERSITY	Bloomington, IN
+M.S., Information Systems	 June 2010
+•	Leadership: VP of Membership, IT Service Management Forum
+
+INDIANA UNIVERSITY	Bloomington, IN
+B.A., Business	 June 2009
+•	Honors: Recipient of merit-based partial scholarship; Peer Tutor of the Month, Computers in Business
+•	Leadership: President, Kelley Consulting Group; Teaching Assistant, Business Law; Treasurer, IU Brazilian Jujitsu Club; Chief of Staff, Foster International Living-Learning Center
+
+ADDITIONAL
+•	Language: Native in English, Minimum Professional Proficiency in Spanish, Elementary Proficiency in Gujarati
+•	Volunteer: Big, Big Brothers Big Sisters; Construction, Habitat for Humanity; Docent, Wonderlab Museum of Science
+•	Hobbies: Reading international literature, drinking single-origin espresso, learning about Persian history
+`;
+
+const consultingJobDescription = `As a Technology Consulting Manager, you’ll help our Financial Services clients transform their technology landscape. You will work alongside Business and IT stakeholders, leading the planning, design and implementation of key IT Change Programmes and advising on emerging technologies.
+
+Examples of projects Technology Consulting Practitioners work on are:
+
+Defining the roadmap for key strategic and legislative driven IT Programmes
+Shaping and leading the delivery of complex technology solutions
+Defining the way our clients exploit the “New IT”: Agile, DevOps, AI, Digital, etc
+Helping clients navigate its journey into the cloud
+Driving the modernisation of legacy IT platforms
+Improving client detection of and response to cybersecurity threats Advising our clients and developing communities of practice on emerging technologies such as Artificial Intelligence, Robotics, Analytics and Blockchain
+
+Basic Qualifications:
+Minimum 5 years Project Management experience
+Minimum 2:1 undergraduate degree, or equivalent.
+Key Experience:
+Knowledge of various methodologies: waterfall, lean, agile
+Understanding team strengths, with experience coaching and developing others
+Experience in stakeholder management and workshop facilitation an advantage
+Experience within Financial Services industries.
+Professional Skill Requirements:
+Experience identifying a client’s high-level business needs and leading the creation of project deliverables (business requirements, functional specs, process flows, use cases, requirements traceability matrices etc)
+Experience leading teams and projects to drive successful outcomes
+Well-developed analytical skills and the ability to synthesize large amounts of information, provide clarity to complex issues, and develop and clearly communicate solutions
+Experience in a functional and/or technical lead role in a business or systems delivery environment
+Experience understanding business challenges and explaining how our offerings and services provide customer value
+Experience working through organizational change, with a proven track record of continuing responsibilities, creativity and innovation, including evidence of solution design
+Good interpersonal, team building, organizational and motivational skills
+An awareness of key delivery methodologies, approaches and market trends in the industry
+Self-starter with an ability to get up-to-speed in a short space of time`;
+
+const projectManagerJobDescription = `Job Description
+
+The Creative Lab at FranklinCovey is hiring a UX Researcher, Project Manager responsible for managing the design solutions including interfaces, user flows and data models and print solutions to meet customer needs and project goals. The person in this role will have a deep understanding and application of methodologies necessary to create experiences that meet and exceed customer expectations this includes but not limited to design thinking and extensive user research for both digital and print use.
+
+The UX Researcher, Project Manager will possess the ability to work and collaborate with the Art Director of UX/UI and Art Director of Print to develop user & process flows, sketches, wireframes, rapid prototypes and detailed user experience specifications for our product suite including corporate and public websites, mobile sites, apps, and marketing/curriculum print materials.
+
+The responsibilities of this position include conducting research including user testing creating solution design & specifications and working closely with our technology team, architects and design product teams to guide features and new product to production regardless of their specific modalities.
+
+RESPONSIBILITIES
+• Oversee project scope, goals and deliverables that support business goals in collaboration with senior management and stakeholders.
+• Maintain project milestone schedules and deliverables.
+• Work collaboratively to manage changes in project scope and direction in order to quickly re-calibrate schedules and budgets.
+• Create inspiring, high converting experiences that lead to repeat purchases
+• Develop customer empathy by regularly conduct user research including testing of new features and products
+• Create rapid prototypes for testing and presentation
+• Build detailed user experience specifications and prototypes that will be used as a base for technical development
+• Produce storyboards, user and work flow documentation & use case scenarios
+• Ensure first class user experience across all of our products
+• In partnership with management, product, tech and marketing, help to define site architecture, product information architecture, interface requirements & navigation flows
+• Collaborate closely with product, tech, marketing, sales and creative design to create elegant user interfaces and rich user experiences
+
+Job Requirements
+
+REQUIREMENTS/QUALIFICATIONS
+• Bachelor’s Degree plus a minimum of 3-5 years of experience in a project management capacity
+• 3+ years in a User Experience role creating experiences that drive conversion and AOV
+• Proven experience conducting research including user testing, synthesizing feedback and results to create best in class experiences
+• Professional appearance and persona
+• Computer proficiency in Adobe Acrobat Pro, MS Office are essential. Understanding of Workfront is helpful
+• Ability to work collaboratively and flexibly with a team in an environment of frequent change
+• Deep understanding of customer needs and how to effectively incorporate them into design
+• Thorough understanding and application of established best practices and methodologies including design thinking to solve complex challenges
+• Knowledge of cutting-edge design conventions and interactions and how to apply them
+• History of defining application functionality and creating interfaces with and in the absence of business requirements
+• Experience with tools such as, adobe XD, and other replay tools helpful
+• Demonstrated ability to document complex designs
+• Mobile (native application and website optimization) and content management solution experience a plus`;
+
+const resumeSample = 'css js node express.js react postgres';
+const jobDescriptionSample = 'css js flexbox softSkill is a';
+
+module.exports = {
+  resumeSample,
+  jobDescriptionSample,
+  adilResume,
+  consultingJobDescription,
+  projectManagerJobDescription
+};
